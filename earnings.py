7b6864afb948ba10e2f0beb08121039d49a674b2
Roshan Shah <roshanshah2007@gmail.com>
2026-06-18 01:00:22 -0400

    Pulls & Calculates earnings data for each stock

---
+++ earnings.py
@@ -0,0 +1,21 @@
+'''
+Stocks Earnings Dashboard - Earnings Pipeline
+Pulls earnings dates, EPS estimates vs actuals for each stock.
+Calculates surprise % = how much a company beat or missed analyst expectations.
+'''
+import yfinance as yf
+import pandas as pd
+from data_pull import STOCKS
+
+def get_earnings_data(ticker):
+    stock = yf.Ticker(ticker)
+    earnings = stock.earnings_dates
+    earnings = earnings.dropna(subset = ['EPS Estimate', 'Reported EPS'])
+
+    # Calculate surprise percentage for more accurate depiction of earnings performance 
+    earnings = earnings.copy()
+    earnings['surprise_pct'] = (earnings['Reported EPS'] - earnings['EPS Estimate']) / earnings['EPS Estimate'] * 100
+    earnings['Ticker'] = ticker
+    return earnings
+
+print(get_earnings_data("AAPL"))
